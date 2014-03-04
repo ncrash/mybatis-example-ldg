027a10f2e6d31e86c39d54a0fea250a49ba2e12c
Daekwon Kang <daekwon.kang@gmail.com>
2014-03-04 10:42:56 +0900

    mybatis-example-web build시 컴파일 에러 발생하는 클래스 주석처리

--- mybatis-example-web/src/main/java/ldg/mybatis/repository/mapper/CommentSqlProvider.java
+++ mybatis-example-web/src/main/java/ldg/mybatis/repository/mapper/CommentSqlProvider.java
@@ -1,20 +1,23 @@
 package ldg.mybatis.repository.mapper;
 
-import static org.apache.ibatis.jdbc.SqlBuilder.BEGIN;
-import static org.apache.ibatis.jdbc.SqlBuilder.FROM;
-import static org.apache.ibatis.jdbc.SqlBuilder.SELECT;
-import static org.apache.ibatis.jdbc.SqlBuilder.SQL;
-import static org.apache.ibatis.jdbc.SqlBuilder.WHERE;
+// import static org.apache.ibatis.jdbc.SqlBuilder.BEGIN;
+// import static org.apache.ibatis.jdbc.SqlBuilder.FROM;
+// import static org.apache.ibatis.jdbc.SqlBuilder.SELECT;
+// import static org.apache.ibatis.jdbc.SqlBuilder.SQL;
+// import static org.apache.ibatis.jdbc.SqlBuilder.WHERE;
 
 import java.util.Map;
 
 public class CommentSqlProvider {
+	
+	// TODO 컴파일 에러 발생한 문제 원인 파악 및 주석처리 제거
 	public String selectCommentByCondition(Map<String, Object> condition) {
-		BEGIN();
-		SELECT("comment_no, user_id, comment_content, reg_date");
-		FROM("comment");
-		WHERE("comment_no = #{commentNo}");
-
-		return SQL();
+		// BEGIN();
+		// SELECT("comment_no, user_id, comment_content, reg_date");
+		// FROM("comment");
+		// WHERE("comment_no = #{commentNo}");
+        // 
+		// return SQL();
+		return null;
 	}
 }
