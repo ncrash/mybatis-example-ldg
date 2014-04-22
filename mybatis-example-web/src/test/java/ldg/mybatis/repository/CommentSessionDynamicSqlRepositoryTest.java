97c6b28548ecb8553703d0d8f8721fee89fd81c6
Daekwon Kang <daekwon.kang@gmail.com>
2014-04-22 11:18:33 +0900

    CommentSessionDynamicSqlRepositoryTest refactoring 중 : java main test -> JUnit test case

--- mybatis-example-web/src/test/java/ldg/mybatis/repository/CommentSessionDynamicSqlRepositoryTest.java
+++ mybatis-example-web/src/test/java/ldg/mybatis/repository/CommentSessionDynamicSqlRepositoryTest.java
@@ -2,61 +2,86 @@ package ldg.mybatis.repository;
 
 import java.util.*;
 
+import ldg.mybatis.model.Comment;
 import ldg.mybatis.model.User;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.junit.Assert.fail;
 
 public class CommentSessionDynamicSqlRepositoryTest {
 	private final CommentSessionDynamicSqlRepository commentSessionDynamicSqlRepository = new CommentSessionDynamicSqlRepository();
 
-	public static void main(String[] args) {
-		CommentSessionDynamicSqlRepositoryTest test = new CommentSessionDynamicSqlRepositoryTest();
-
-		test.testSelectCommentByCondition();
-		test.testSelectCommentByConditionIf();
-		test.testSelectCommentByConditionChoose();
-		test.testSelectCommentByConditionTrim();
-		test.testSelectCommentByConditionForeach();
-		test.testSelectOgnlStaticField();
-		test.testSelectOgnlStaticMethod();
-		test.testSelectOgnlStaticMethod2();
-	}
-
+    @Test
 	public void testSelectCommentByCondition() {
 		Map<String, Object> condition = new HashMap<String, Object>();
 		condition.put("commentNo", 1L);
 		condition.put("user", makeUser());
 
-		commentSessionDynamicSqlRepository.selectCommentByCondition(condition);
-	}
+        List<Comment> comments = commentSessionDynamicSqlRepository.selectCommentByCondition(condition);
 
+        assertThat(comments.size(), greaterThanOrEqualTo(1));
+    }
+
+    @Test
 	public void testSelectCommentByConditionIf() {
 		Map<String, Object> condition = new HashMap<String, Object>();
 		condition.put("commentNo", 1L);
 		condition.put("user", makeUser());
 
 		commentSessionDynamicSqlRepository.selectCommentByConditionIf(condition);
+        fail();
 	}
 
+    @Test
 	public void testSelectCommentByConditionChoose() {
 		Map<String, Object> condition = new HashMap<String, Object>();
 		condition.put("commentNo", 1L);
 		condition.put("user", makeUser());
 
 		commentSessionDynamicSqlRepository.selectCommentByConditionChoose(condition);
+        fail();
 	}
 
+    @Test
 	public void testSelectCommentByConditionTrim() {
 		Map<String, Object> condition = new HashMap<String, Object>();
 		condition.put("commentNo", 1L);
 		condition.put("user", makeUser());
 
 		commentSessionDynamicSqlRepository.selectCommentByConditionTrim(condition);
+        fail();
 	}
 
+    @Test
 	public void testSelectCommentByConditionForeach() {
 		Map<String, Object> condition = new HashMap<String, Object>();
 		condition.put("commentNos", Arrays.asList(1L, 2L));
 
 		commentSessionDynamicSqlRepository.selectCommentByConditionForeach(condition);
+        fail();
+	}
+
+    @Test
+	public void testSelectOgnlStaticField() {
+		commentSessionDynamicSqlRepository.selectOgnlStaticField();
+        fail();
+	}
+
+    @Test
+	public void testSelectOgnlStaticMethod() {
+		commentSessionDynamicSqlRepository.selectOgnlStaticMethod();
+        fail();
+	}
+
+    @Test
+	public void testSelectOgnlStaticMethod2() {
+		Map<String, Object> condition = new HashMap<String, Object>();
+		condition.put("userId", "fromm0");
+
+		commentSessionDynamicSqlRepository.selectOgnlStaticMethod2(condition);
+        fail();
 	}
 
 	private User makeUser() {
@@ -64,20 +89,5 @@ public class CommentSessionDynamicSqlRepositoryTest {
 		user.setUserId("fromm0");
 		user.setUserName("이동국");
 		return user;
-	}
-
-	public void testSelectOgnlStaticField() {
-		commentSessionDynamicSqlRepository.selectOgnlStaticField();
-	}
-
-	public void testSelectOgnlStaticMethod() {
-		commentSessionDynamicSqlRepository.selectOgnlStaticMethod();
-	}
-
-	public void testSelectOgnlStaticMethod2() {
-		Map<String, Object> condition = new HashMap<String, Object>();
-		condition.put("userId", "fromm0");
-
-		commentSessionDynamicSqlRepository.selectOgnlStaticMethod2(condition);
 	}
 }
