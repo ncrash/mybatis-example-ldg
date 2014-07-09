abeb4736c9ecf2d8a34f83a46b8e48864e616163
Daekwon Kang <daekwon.kang@gmail.com>
2014-07-09 16:05:45 +0900

    gradle build UTF-8 적용, SqlBuilder Deprecated 된것 권고안으로 변경

--- mybatis-example-web/src/main/java/ldg/mybatis/repository/mapper/CommentSqlProvider.java
+++ mybatis-example-web/src/main/java/ldg/mybatis/repository/mapper/CommentSqlProvider.java
@@ -1,21 +1,16 @@
 package ldg.mybatis.repository.mapper;
 
-import static org.apache.ibatis.jdbc.SqlBuilder.BEGIN;
-import static org.apache.ibatis.jdbc.SqlBuilder.FROM;
-import static org.apache.ibatis.jdbc.SqlBuilder.SELECT;
-import static org.apache.ibatis.jdbc.SqlBuilder.SQL;
-import static org.apache.ibatis.jdbc.SqlBuilder.WHERE;
+import org.apache.ibatis.jdbc.SQL;
 
 import java.util.Map;
 
 public class CommentSqlProvider {
 	
-	public String selectCommentByCondition(Map<String, Object> condition) {
-		BEGIN();
-		SELECT("comment_no, user_id, comment_content, reg_date");
-		FROM("comment");
-		WHERE("comment_no = #{commentNo}");
-
-		return SQL();
+    public String selectCommentByCondition(final Map<String, Object> condition) {
+        return new SQL() {{
+            SELECT("comment_no, user_id, comment_content, reg_date");
+            FROM("comment");
+            WHERE("comment_no = #{commentNo}");
+        }}.toString();
 	}
 }
